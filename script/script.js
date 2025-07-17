6cfb592da92eada58a5c2914da4f911e229d91c6
kauan <kauan7.pa.krlh@gmail.com>
2025-07-17 20:19:48 -0300

    initial commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+BtnReset.addEventListener("click" , ()=>{
+    document.getElementById('inputUs').value = ""
+});
+
+btnConverted.addEventListener("click" , ()=>{
+    let InputUs = document.getElementById('inputUs').value
+        console.log(InputUs)
+})
